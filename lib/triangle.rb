c80f2dd9936a78ecab29551eb144a5c3536c92bb
Sachin Mahajan <sachinm78@gmail.com>
2019-03-27 23:17:08 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -11,6 +11,9 @@ class Triangle
         raise TriangleError
       elsif (side_1 + side_2 <= side_3) || (side_1 + side_3 <= side_2) || (side_2 + side_3 <= side_1)
         raise TriangleError
+      else
+        if (side_1 == side_2) || (side_2 == side_3) || (side_1 == side_3)
+          :eq
       end
   end
 
