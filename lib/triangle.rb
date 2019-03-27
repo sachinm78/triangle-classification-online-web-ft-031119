16b01bad357347de5eeeb87118ab604fd0d15c78
Sachin Mahajan <sachinm78@gmail.com>
2019-03-27 23:13:45 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,7 +8,8 @@ class Triangle
 
   def kind()
       if (side_1 <= 0)  || (side_2 <= 0) || )side_3 <= 0)
-        raise 
+        raise TriangleError
+      elsif (side_1 + side_2 <)
   end
 
   class TriangleError < StandardError
